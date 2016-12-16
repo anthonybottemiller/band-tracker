dc5f036caca1f53a56297cabefdf99cfd58f9f2e
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-15 22:22:00 -0800

    add test for database empty at first

---
+++ Tests/testBand.cs
@@ -0,0 +1,30 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class BandTest
+  {
+    public BandTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void BandsEmptyAtFirst()
+    {
+      int result = Band.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+
+    // [Fact]
+    // public void Band_Equal_ReturnsTrueIfNamesAreSame_True()
+    // {
+
+    // }
+  }
+}
