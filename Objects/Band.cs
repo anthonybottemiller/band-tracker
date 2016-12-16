321e016118e8d04fb040755c1dbb27872a6d678c
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-15 22:26:06 -0800

    implement getall() method for Band

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -24,5 +24,36 @@ namespace BandTracker
     {
       return _name;
     }
+
+    public static List<Band> GetAll()
+    {
+      List<Band> allBands = new List<Band>{};
+
+      SqlConnection connection = DB.Connection();
+      connection.Open();
+
+      SqlCommand command = new SqlCommand("SELECT * FROM bands;", connection);
+      SqlDataReader reader = command.ExecuteReader();
+
+      while(reader.Read())
+      {
+        int bandId = reader.GetInt32(0);
+        string bandName = reader.GetString(1);
+        Band newBand = new Band(bandName, bandId);
+        allBands.Add(newBand);
+      }
+
+      if (reader != null)
+      {
+        reader.Close();
+      }
+
+      if (connection != null)
+      {
+        connection.Close();
+      }
+
+      return allBands;
+    }
   }
 }
