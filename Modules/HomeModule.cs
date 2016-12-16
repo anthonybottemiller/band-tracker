b8d66c7bf98ebf13f7c688727128ad270f3e0dbe
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-16 10:14:03 -0800

    add "hello world" home module with index.cshtml

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace BandTracker
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
