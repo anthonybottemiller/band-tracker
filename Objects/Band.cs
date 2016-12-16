6b1d298f2f979a45cd875df72d55a24393ca7227
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-15 22:22:31 -0800

    adds barebones Band class with properties and getters

---
+++ Objects/Band.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Band
+  {
+    private int _id;
+    private string _name;
+
+    public Band(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+  }
+}
